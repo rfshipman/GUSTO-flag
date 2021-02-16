c94aa09423ce1c115f0825299a1c4e588c70a310
rfshipman <russ@sron.nl>
2021-02-16 12:19:13 +0100

    Updated initial proto-type

--- gustoFlag.py
+++ gustoFlag.py
@@ -5,33 +5,50 @@ import numpy as np
 from flags import Flags
 
 
-class GustoObsFlag(Flags):
+class GustoSeqFlag(Flags):
     """
     Define flags which reflect the quality or processing of an observation.  Observations refer to a
     calibratable set of integrations (for example a full OTF scan including the HOTs and REFs)
-    """
-    flag1 = 1, 'incomplete_obs'
-    flag2 = 2, 'corrupt_obs'
-    flag3 = 4, 'level_1a_processed'
-    flag4 = 8, 'level_1b_processed'
-    flag5 = 16, 'level_1c_processed'
-    flag6 = 32, 'baseline_corrected'
 
+    usage:
+    # produce a level_1a processed flag
+    level_1a = GustoSeqFlag(level_1a)
+
+    # a processed level flag in header
+
+    processed_flag = processed_level |
+
+    """
+    incomplete = 1, 'incomplete_obs'
+    lavel_1a = 4, 'level_1a_processed'
+    level_1b = 8, 'level_1b_processed'
+    level_1c = 16, 'level_1c_processed'
+    badbaselines = 32, 'baseline_issues'
+
+
+
+class GustoBbFlag(Flags):
+    """
+    Define flags reflecting the quality of an individual integration of a building block type .  This could
+    be for example the quality of a "HOT" integration.
+    """
+    corrupt = 1, 'corrupt_data'
+    problem = 2, 'unknown_problem'
+    ignore = 4, 'ígnore_bb'
+
+
+class GustoMixerFlag(Flags):
+    """
+    Define Flags which impact an individual mixer in the array.
+    """
+    underpumped   = 1, 'mixer_underpumped'
+    flag2         = 2, 'TBD'
 
 class GustoChannelFlag(Flags):
     """
     Define Flags which impact individual spectrometer channels, can appear at any channel of any integration
     """
-    flag1 = 1, 'spike'
-    flag2 = 2, 'out_of_limit'
-    flag3 = 4, 'potential_line'
-    flag4 = 8, 'unknown_problem'
-
-class GustoIntegrationFlag(Flags):
-    """
-    Define flags reflecting the quality of an individual integration.  This could be for example the quality
-    of a "HOT" integration
-    """
-    flag1 = 1, 'corrupt_data'
-    flag2 = 2, 'mixer_under_pumped'
-    flag3 = 4, 'unknown_problem'
+    spike         = 1, 'spike'
+    data_ool      = 2, 'out_of_limit'
+    possible_line = 4, 'potential_line'
+    bad_channel   = 8, 'unknown_problem'
